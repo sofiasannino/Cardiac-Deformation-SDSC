84afcd9b0f14371fc692a2463cb4e9178eec956f
sofiasannino <sofia.sannino@gmail.com>
2026-04-02 16:13:47 +0200

    add requirements

--- pretrain.py
+++ pretrain.py
@@ -8,7 +8,7 @@ from src import convert_acdc_to_nnunet
 @hydra.main(version_base=None, config_path="configs/model", config_name="unet")
 def main(cfg: DictConfig):
 
-    # set up dataset according to unet requirements 
+    # set up dataset according to U-Net requirements 
     convert_acdc_to_nnunet(
         acdc_training_root=cfg.data.acdc_training_root,
         out_dataset_root=cfg.data.out_dataset_root,
